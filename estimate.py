72855138a4afee4bb41f0142c6f98ab62d8e993d
Pranav Padmanabhan <pranavpadmanabhan2501@gmail.com>
2021-05-30 21:35:07 +0530

    function to find value of pi

--- estimate.py
+++ estimate.py
@@ -1,6 +1,13 @@
 import math
 import unittest
 
+def wallis(n):
+  s=1
+  for i in range(1,n+1):
+      s*=(4*i**2)/((4*i**2)-1)
+  return s*2   
+
+
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
